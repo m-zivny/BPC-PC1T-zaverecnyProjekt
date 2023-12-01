48c2bebceb4938d25edf20ea4471efbae27236ba
m-zivny <256818@vut.cz>
2023-12-01 17:00:08 +0100

    missing main.h

---
+++ main.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h> // knihovna pro funkci sleep
+#include <conio.h> // knihovna pro sanzší získávání vstupního inputu z "šipek"
+#include <stdlib.h>
+#include <time.h>
+
+#define RADKY 16
+#define SLOUPCE 32
+char pole[RADKY][SLOUPCE];
+
+time_t cas;
+time_t timestamp;
+time_t rozdil;
+bool tempDirection;
+
+struct bodyPart {
+
+	int posX = 0;
+	int posY = 0;
+	bodyPart* next = NULL;
+
+};
+
+enum Direction {
+	UP = 1,
+	DOWN,
+	LEFT,
+	RIGHT
+};
